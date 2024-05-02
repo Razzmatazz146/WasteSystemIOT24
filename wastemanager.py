bc586c8d0bb194fc7e1adcdc7e82540dbbadcc7b
Willuku <le1952006@crc-lennox.qc.ca>
2024-05-02 15:07:29 -0400

    code check implementation

--- wastemanager.py
+++ wastemanager.py
@@ -1,6 +1,8 @@
+import time
+
 EMPTY = 0
 FULL = 1
-CODE = "R, B, B, R idk"
+PASSCODE = ['R', 'B', 'B', 'R']
 
 # in real time, gets the distance from the sensor to the surface (bottom of container or waste) 
 # and returns the current_level. Probably have a way in this function to check if the container
@@ -19,9 +21,33 @@ def update_visuals(current_level):
     pass
 
 # that returns is_unlocked if provided sequence is correctly given within a 4 second span.
-# Compares to a provided CODE.
-def code_check():
- pass
+# Compares to a provided PASSCODE.
+def code_check(button):
+    global PASSCODE
+    
+    # Puts the initial button pressed into the input array
+    input = [button]
+    
+    # Starts the timer
+    start = time.monotonic()
+    
+    # For loop that expects the next 3 inputs
+    for i in range(3):
+        if 'red button':
+            input.append('R')
+        elif 'blue button':
+            input.append('B')
+    
+    # Stops the timer
+    end = time.monotonic()
+    
+    # Checks if time elapsed is greater than 4 seconds
+    if end - start > 4:
+        return False
+    elif input != PASSCODE:
+        return False
+    else:
+        return True
 
 #that buzzes the alarm for as long as it is receiving true. 
 def start_alarm():
